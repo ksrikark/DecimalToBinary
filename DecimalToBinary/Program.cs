ec64af9db362383da0e1fc7d403b55681aa2aa42
ksrik <ksrik@MURDOC>
2022-07-19 21:04:08 +0530

    Add project files.

---
+++ DecimalToBinary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+int n = 5;
+var digits = new List<int>();
+while (n > 0)
+{
+    digits.Add(n % 2);
+    n = n / 2;
+}
+for (int i = digits.Count - 1; i >= 0; i--)
+{
+    Console.Write(digits[i]);
+}
+
